f8c4a2c33e8b3a086081a5670aefa155c5b0c354
unknown <ernestoroja@ufl.edu>
2023-04-22 15:19:38 -0400

    implemented states

---
+++ states/gameOver.js
@@ -0,0 +1,17 @@
+export class gameOver extends Phaser.Scene {
+    constructor() {
+        super({ key: 'GameOver' });
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        console.log("IN GAMEOVER");
+    }
+    
+    update() {
+
+    }
+}
---
+++ states/menu.js
@@ -0,0 +1,17 @@
+export class menu extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MainMenu' });
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        console.log("IN MENU");
+    }
+    
+    // update() {
+    //     console.log("TEST");
+    // }
+}
